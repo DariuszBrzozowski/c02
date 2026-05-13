494e755db5f940ae9f61aa0df0eeb8cc3a8351f1
Dariusz <dbrzozowski@poczta.onet.pl>
2026-05-13 22:57:36 +0200

    correct ex09

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -2,27 +2,26 @@
 
 char	*ft_strcapitalize(char *str)
 {
-	int	i;
+	char	*start;
+	int	new_word;
 
-	i = 0;
-	
+	start = str;
+	new_word = 1;
 	while (*str != '\0')
-	{	
-		if (i == 0 && *str >= 'a' && *str <= 'z')
-			*str = *str + 'A' - 'a';
-		else if (i > 0 && *str >= 'A' && *str <= 'Z')
-			*str = *str + 'a' - 'A';
-		if (*str >= 'a' && *str <= 'z')
-			i++;
-		else if (*str >= 'A' && *str <= 'Z')
-			i++;
-		else if (*str >= '0' && *str <= '9')
-			i++;
-		else
-			i = 0;
+	{
+		if ((*str >= 'a' && *str <= 'z') || (*str >= 'A' && *str <= 'Z'))
+		{
+			if (new_word && *str >= 'a' && *str <= 'z')
+				*str = *str - 'a' + 'A';
+			else if (!new_word && *str >= 'A' && *str <= 'Z')
+				*str = *str - 'A' + 'a';
+			new_word = 0;
+		}
+		else if (!(*str >= '0' && *str <= '9'))
+			new_word = 1;
 		str++;
 	}
-	return (str);
+	return (start);
 }
 /*
 int main(void)
