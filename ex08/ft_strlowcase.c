66159fd1a2f49976140d2032eb1572289275246e
Dariusz <dbrzozowski@poczta.onet.pl>
2026-05-14 10:41:12 +0200

    added testing for ex08

--- ex08/ft_strlowcase.c
+++ ex08/ft_strlowcase.c
@@ -1,19 +1,41 @@
-/*#include <stdio.h>*/
+char	*ft_strcpy(char *dest, char *src)
+{
+	char	*temp;
+	
+	temp = dest;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (temp);
+}
 
 char	*ft_strlowcase(char *str)
 {
-	
+	char	*temp;
+	temp = str;
+
 	while (*str != '\0')
 	{	if (*str >= 'A' && *str <= 'Z')
-			*str = *str + 'a' - 'A';
+			*str = *str -'A' + 'a';
 		str++;
 	}
-	return (str);
+	return (temp);
 }
-/*
-int main(void)
+
+int main(int argc, char *argv[])
 {
-	char s[] = "dariusz!";
-	printf("%d", ft_strlowcase(s));
+	#include <stdio.h>
+
+	char s[256];
+
+	if (argc > 1)
+	{
+		ft_strcpy(s, argv[1]);
+		printf("%s", ft_strlowcase(s));
+	}
 	return (0);
-}*/
+}
