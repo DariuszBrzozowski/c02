023d5cebd35b71fe9438a4c8c8dd3681a1d93991
Dariusz <dbrzozowski@poczta.onet.pl>
2026-05-14 09:50:54 +0200

    added testing from cli to ex05

--- ex05/ft_str_is_uppercase.c
+++ ex05/ft_str_is_uppercase.c
@@ -1,22 +1,21 @@
-/*#include <stdio.h>*/
+#include <stdio.h>
 
 int	ft_str_is_uppercase(char *str)
 {
-	int	out;
-
-	out = 1;
-	while (*str != '\0' && out == 1)
-	{
-		if (!(*str >= 'A'  && *str <= 'Z'))
-			out = 0;
+	while (*str =! '\0'){
+		if (!(*str >= 'A' && *str <= 'Z'))
+			return (0);
 		str++;
 	}
-	return (out);
+	return (1);
+
 }
-/*
-int main(void)
+
+int main(int argc, char *argv[])
 {
-	char s[] = "dariusz!";
-	printf("%d", ft_str_is_upppercase(s));
+	if (argc > 1)
+	{
+		printf("%d", ft_str_is_uppercase(argv[1]));
+	}
 	return (0);
-}*/
+}
