2625a9e41465e4d68bd7a9c116d23d2748f77ae8
Dariusz <dbrzozowski@poczta.onet.pl>
2026-05-14 10:37:23 +0200

    added testing for ex07

--- ex07/ft_strupcase.c
+++ ex07/ft_strupcase.c
@@ -1,19 +1,41 @@
-/*#include <stdio.h>*/
+char	*ft_strcpy(char *dest, char *src)
+{
+	char	*temp;
+	
+	temp = dest;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+	return (temp);
+}
 
 char	*ft_strupcase(char *str)
 {
-	
+	char	*temp;
+	temp = str;
+
 	while (*str != '\0')
 	{	if (*str >= 'a' && *str <= 'z')
-			*str = *str + 'A' - 'a';
+			*str = *str -'a' + 'A';
 		str++;
 	}
-	return (str);
+	return (temp);
 }
-/*
-int main(void)
+
+int main(int argc, char *argv[])
 {
-	char s[] = "dariusz!";
-	printf("%d", ft_str_is_upppercase(s));
+	#include <stdio.h>
+
+	char s[256];
+
+	if (argc > 1)
+	{
+		ft_strcpy(s, argv[1]);
+		printf("%s", ft_strupcase(s));
+	}
 	return (0);
-}*/
+}
